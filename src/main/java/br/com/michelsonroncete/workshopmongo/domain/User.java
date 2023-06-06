a567a6ee0d684cdf8bb9c0b36f4586a2168e6384
Michelson Roncete <michelsonroncete@gmail.com>
2023-06-05 23:04:49 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/michelsonroncete/workshopmongo/domain/User.java
+++ src/main/java/br/com/michelsonroncete/workshopmongo/domain/User.java
@@ -12,27 +12,19 @@ public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	private String name;
 	private String id;
+	private String name;
 	private String email;
 	
 	public User() {
 		
 	}
 
-	public User(String name, String id, String email) {
+	public User(String id, String name, String email) {
 		super();
-		this.name = name;
 		this.id = id;
-		this.email = email;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
 		this.name = name;
+		this.email = email;
 	}
 
 	public String getId() {
@@ -41,6 +33,14 @@ public class User implements Serializable{
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getEmail() {
---
+++ src/main/java/br/com/michelsonroncete/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package br.com.michelsonroncete.workshopmongo.resources.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.michelsonroncete.workshopmongo.services.exception.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontardo", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
