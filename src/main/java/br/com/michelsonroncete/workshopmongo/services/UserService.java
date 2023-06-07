7d9b54913b8a82972888e37270314d530dc59797
Michelson Roncete <michelsonroncete@gmail.com>
2023-06-06 21:31:45 -0300

    Inserção de usuário com POST

--- src/main/java/br/com/michelsonroncete/workshopmongo/services/UserService.java
+++ src/main/java/br/com/michelsonroncete/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.michelsonroncete.workshopmongo.domain.User;
+import br.com.michelsonroncete.workshopmongo.dto.UserDTO;
 import br.com.michelsonroncete.workshopmongo.repository.UserRepository;
 import br.com.michelsonroncete.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,11 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
