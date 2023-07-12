8303328a2d8835ee5868cc9bf68e527c7b3d8d75
Michelson Roncete <michelsonroncete@gmail.com>
2023-07-12 09:06:36 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/michelsonroncete/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package br.com.michelsonroncete.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.michelsonroncete.workshopmongo.domain.Post;
+import br.com.michelsonroncete.workshopmongo.repository.PostRepository;
+import br.com.michelsonroncete.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+}
